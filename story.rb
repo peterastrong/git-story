55eb2ea1e75b9e3bc2592891c82aa27f7e00d253
Peter Strong <peter.strong@gmail.com>
2017-01-08 14:08:39 -0500

    add a not on thoughts about the story

--- story.rb
+++ story.rb
@@ -2,3 +2,5 @@ I am writing a story
 it is several lines long 
 and is lots of fun 
 but I am just starting out as a writer so it cannot be that great 
+
+maybe? it would be better if I took the story in another direction 
